9e22d9dd95f3a37ceaddc1fb55ed78286d6a261a
Prahlad Gaur <65204011+prahladgaur@users.noreply.github.com>
2024-05-05 17:46:54 +0530

    Update HeroImage.jsx

--- src/components/HeroImage/HeroImage.jsx
+++ src/components/HeroImage/HeroImage.jsx
@@ -3,7 +3,7 @@ import heroImage from "../../assets/images/Influencer.png";
 
 const HeroImage = () => (
   <div className="hero-image" style={{ flex: 1 }}>
-    <img src={heroImage} alt="Hero" style={{ width: "100%", height: "auto",marginLeft:"auto",marginRight:"auto"}} />
+    <img src={heroImage} alt="Hero" style={{ width: "75%", height: "auto",marginLeft:"auto",marginRight:"auto"}} />
   </div>
 );
 
